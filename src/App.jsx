22bdcff1aee035e663fdff465603e52d3af341b1
Mariangelica <gromarant@gmail.com>
2025-01-05 23:06:51 +0100

    build and deploy app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Main from './layout/Main.jsx';
 import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Main/>
-  </BrowserRouter>
+  </HashRouter>
 )
